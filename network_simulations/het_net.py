580966d9d61807e0194acb26cd34c7a13259d967
pgregory <pdlaxin@gmail.com>
2019-11-06 16:46:17 +0100

    more work to simulation

--- network_simulations/het_net.py
+++ network_simulations/het_net.py
@@ -5,21 +5,20 @@ class het_network():
 
         self.coverage_area = (100, 100)
         self.base_stations = []
-        num_femto_users, num_antenna = 3, 3
+        num_femto_users, num_antenna = 1,1
         # these loops should probably be moved out of the constructor
-        [self.base_stations.append(femto_cell(i, self, num_femto_users, num_antenna)) for i in range(num_femto_cells)]
+        [self.base_stations.append(femto_base_station(i, self, num_femto_users, num_antenna)) for i in range(num_femto_cells)]
 
         self.macro_users = []
         interference_threshold = 5
         [self.macro_users.append(macro_user(i, self, interference_threshold)) for i in range(num_macro_users)]
         self.update_macro_cells()
 
-
     def get_femto_cells(self):
         return self.base_stations
 
     def allocate_power(self):
-        return None
+        [player.allocate_femto_users_power() for player in self.base_stations]
 
     def update_macro_cells(self):
         for cell in self.base_stations:
@@ -33,8 +32,8 @@ class het_network():
     def move_macro_users(self):
         return None
 
-class femto_cell():
-    def __init__(self, ID, network, num_femto_users, num_antenna):
+class femto_base_station():
+    def __init__(self, ID, network, num_femto_users, num_antenna,utility_function=np.sum):
         self.ID = ID
         self.users = []
         self.number_antennas = num_antenna
@@ -44,7 +43,8 @@ class femto_cell():
         self.coverage_size = np.array((5, 5))
         self.connect_users(num_femto_users)
         self.move_femto_users()
-
+        self.utility_function = utility_function
+        self.utility_evaluated = self.update_utility()
 
 
     #TODO type this parameter as macro user
@@ -91,6 +91,21 @@ class femto_cell():
         power = 1
         for user in self.users:
             user.update_power(power)
+        self.update_utility()
+
+    def update_utility(self):
+        """
+
+        :param utility_function: This should be a non-decreasing concave function
+        :return:
+        """
+        self.utility_evaluated = self.utility_function(self.get_user_sinr())
+
+    def get_user_sinr(self):
+        all_sinr = []
+        for user in self.users:
+            all_sinr.append(user.get_sinr())
+        return all_sinr
 
 #TODO if further types of users are added, make a user class and inherit from this
 
@@ -120,21 +135,28 @@ class macro_user():
 
 
 class femto_user():
-    def __init__(self, ID, parent):
+    def __init__(self, ID, parent, sigma_square=1):
         self.parent = parent
         self.ID = ID
         self.power_from_base_station = 0
-        self.SINR = 0
         self.uplink_channel = 0
         self.downlink_channel = 0
+        self.interference = 0
+        self.noise_power = sigma_square
         self.location = 0
+        self.SINR = (self.downlink_channel*self.power_from_base_station)/(self.noise_power+self.interference)
 
     def move(self):
         test = 0
         self.location = self.parent.location  - self.parent.coverage_size/2 + \
-            [np.random.randint(0,self.parent.coverage_size[0]), np.random.randint(0,self.parent.coverage_size[1])]
+            [np.random.randint(0, self.parent.coverage_size[0]), np.random.randint(0,self.parent.coverage_size[1])]
         self.uplink_channel = np.random.randn()
         self.downlink_channel = np.random.randn()
 
     def update_power(self, power):
         self.power_from_base_station = power
+        self.SINR = (self.downlink_channel*self.power_from_base_station)/(self.noise_power+self.interference)
+
+
+    def get_sinr(self):
+        return self.SINR
--- tests/single_antenna_user_test.py
+++ tests/single_antenna_user_test.py
@@ -1,6 +1,7 @@
 from network_simulations import het_net
 
 def test_basic_network_setup():
-    network = het_net.het_network(3, 3)
+    network = het_net.het_network(10, 10)
+    network.allocate_power()
 
     assert False
